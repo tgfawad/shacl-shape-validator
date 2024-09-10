8932a7d5a0b85dda1822a15ef278e574dc107786
tgfawad <tgfawad@gmail.com>
2024-09-10 12:09:10 +0200

    using writable temporary files

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import tempfile
+
 from debugpy.common.timestamp import reset
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -26,8 +28,6 @@ def validate():
     shapes_data = request.json.get('shapes_data')
     graph_data = request.json.get('data')
 
-    os.makedirs('/data', exist_ok=True)
-
     if not shapes_data or not graph_data:
         return jsonify({"error": "Both shapes_file and data_file are required."}), 400
 
@@ -35,27 +35,29 @@ def validate():
     shapes_data = shapes_data.replace('\r', '')
     graph_data = graph_data.replace('\r', '')
     # Save the RDF shape data to temporary files
-    shapes_file_path = '/data/shapes.ttl'
-    with open(shapes_file_path, 'w', encoding='utf-8') as shapes_file:
-        shapes_file.write(shapes_data)
+    # Create a temporary directory
+    with tempfile.TemporaryDirectory() as temp_dir:
+        shapes_file_path = os.path.join(temp_dir, 'shapes.ttl')
+        data_file_path = os.path.join(temp_dir, 'datagraph.ttl')
+        with open(shapes_file_path, 'w', encoding='utf-8') as shapes_file:
+            shapes_file.write(shapes_data)
 
-    # Save the RDF graph data to temporary files
-    data_file_path = '/data/datagraph.ttl'
-    with open(data_file_path, 'w', encoding='utf-8') as data_file:
-        data_file.write(graph_data)
+        # Save the RDF graph data to temporary files
+        with open(data_file_path, 'w', encoding='utf-8') as data_file:
+            data_file.write(graph_data)
 
-    # Command to run pyshacl
-    command = [
-        'python', '-m', 'pyshacl',
-        '-s', shapes_file_path, # Shapes file
-        '-d', data_file_path, # Data file
-        '-f', 'turtle' # return format
-    ]
+        # Command to run pyshacl
+        command = [
+            'python', '-m', 'pyshacl',
+            '-s', shapes_file_path, # Shapes file
+            '-d', data_file_path, # Data file
+            '-f', 'turtle' # return format
+        ]
 
-    # Run the command and capture the output
-    result = subprocess.run(command, capture_output=True, text=True)
+        # Run the command and capture the output
+        result = subprocess.run(command, capture_output=True, text=True)
 
-    print(result)
+        print(result)
     # Return the output
     return jsonify({
         "stdout": result.stdout,
